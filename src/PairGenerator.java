dd80301065a382f7d7d104b583cbd4ed9a1a445a
Amir Makhmudov <amir.makhmudov.f@gmail.com>
2025-11-28 12:13:12 +0300

    feat: выполнить задание

---
+++ src/PairGenerator.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class PairGenerator {
+    private static final Random rnd = new Random();
+
+    /**
+     * Разбивает список незнакомцев по парам.
+     */
+    public List<List<Stranger>> splitByPairs(List<Stranger> strangers) {
+        List<List<Stranger>> pairs = new ArrayList<>();
+        while (!strangers.isEmpty()) {
+
+            List<Stranger> pair = getRandomPair(strangers);
+            pairs.add(pair);
+        }
+
+        return pairs;
+    }
+
+    /**
+     * Возвращает одну пару и удаляет её из списка strangers
+     */
+    private List<Stranger> getRandomPair(List<Stranger> strangers) {
+
+        // Генерируем два случайных индекса в пределах списка
+        int p1Index = rnd.nextInt(strangers.size());
+        int p2Index = rnd.nextInt(strangers.size());
+        while (p2Index == p1Index) {
+            p2Index = rnd.nextInt(strangers.size());
+        }
+
+        // Получаем элементы по сгенерированным индексам
+        Stranger strangerOne = strangers.get(p1Index);
+        Stranger strangerTwo = strangers.get(p2Index);
+
+        strangers.removeAll(List.of(strangerOne, strangerTwo));
+
+        return List.of(strangerOne, strangerTwo);
+    }
+}
